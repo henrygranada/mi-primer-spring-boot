ea93b14a7f928d91ab2fe02df90680cd92608066
henrygranada <94702711+henrygranada@users.noreply.github.com>
2022-03-21 13:04:05 -0500

    Update PersonaController.java
    mi primer cambio

--- src/main/java/com/example/democrudmongo/controller/PersonaController.java
+++ src/main/java/com/example/democrudmongo/controller/PersonaController.java
@@ -40,7 +40,7 @@ public class PersonaController {
 	 }
 	 
 	 @GetMapping(value="/delete/{id}")
-	 /*clase de eliminacion nueva*/
+	 /*clase de eliminacion nueva y desde 0*/
 	 public ResponseEntity<Persona>delete(@RequestBody Long id){
 		 Persona persona = personaServiceApi.get(id);
 		 if(persona != null) {
